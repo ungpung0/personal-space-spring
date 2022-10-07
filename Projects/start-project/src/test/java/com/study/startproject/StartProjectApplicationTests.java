f59e6c2bf224635682aeb02cd9d5e00139add1a2
ungpung0 <ungpung0@gmail.com>
2022-10-07 11:30:54 +0900

    Commit start-project
    초기 세팅 연습 프로젝트 생성 완료.
    간단한 컨트롤러 작성후 테스트 완료.

---
+++ Projects/start-project/src/test/java/com/study/startproject/StartProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.study.startproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("# Test contextLoads");
+	}
+
+}
