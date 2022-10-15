637685697fa3753f9722aef4997d27ea71ae5bfa
ungpung0 <ungpung0@gmail.com>
2022-10-15 18:10:09 +0900

    Commit thymeleaf-project
    게시판 연습 프로젝트 생성 완료.

---
+++ Projects/guestbook-project/src/main/java/com/study/guestbookproject/GuestbookProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.guestbookproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestbookProjectApplication.class, args);
+    }
+
+}
---
+++ Projects/guestbook-project/src/test/java/com/study/guestbookproject/GuestbookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.guestbookproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
