cddcb1948124a0513b9d6e0508f4476e7ae6a89e
ungpung0 <ungpung0@gmail.com>
2022-10-18 21:24:18 +0900

    Commit guestbook-project
    등록 페이지 구현 완료.

--- Projects/guestbook-project/src/main/java/com/study/guestbookproject/controller/GuestbookController.java
+++ Projects/guestbook-project/src/main/java/com/study/guestbookproject/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.study.guestbookproject.controller;
 
+import com.study.guestbookproject.dto.GuestbookDTO;
 import com.study.guestbookproject.dto.PageRequestDTO;
 import com.study.guestbookproject.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -27,6 +30,19 @@ public class GuestbookController {
         log.info("list log");
         model.addAttribute("result", guestbookService.getList(pageRequestDTO));
         // result란 이름으로 목록 리스트 전달.
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register log");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO guestbookDTO, RedirectAttributes redirectAttributes) {
+        log.info("register post log");
+        Long gno = guestbookService.register(guestbookDTO);
+        redirectAttributes.addFlashAttribute("msg", gno); // 모달 창을 띄우는 용도로 사용.
+        return "redirect:/guestbook/list";
     }
 
 }
