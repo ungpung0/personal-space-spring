40828c9365e0de5be658d279f62017253cbc9dd5
ungpung0 <ungpung0@gmail.com>
2022-11-13 11:19:44 +0900

    Commit bimovie
    영화 양방향 참조 프로젝트 생성.

---
+++ Projects/bimovie/src/test/java/com/study/bimovie/BimovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.bimovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BimovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
